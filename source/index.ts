b710e87a61974dd5e22f0aeb4db7a6a33aae2f68
lsunsi <sunsi.lucas@gmail.com>
2017-11-14 10:36:41 -0200

    WIP: first draft implementation done.

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,30 @@
-console.log('hey');
+type Consumer<A> = (a: A) => void;
+type Disable = () => void;
+type Enable<A, B> = (c: Consumer<A>) => B;
+type Sub<A> = Enable<A, Disable>;
+type Subs<A, B> = (a: A) => Sub<B>[];
+
+const Subs = subs => ({dispatch, getState}) => next => {
+	const activeSubs = {};
+
+	return action => {
+		const a = next(action);
+
+		const currentSubs = subs(getState());
+
+		Object.keys(activeSubs).forEach(id => {
+			if (currentSubs[id]) {
+				delete currentSubs[id];
+			} else {
+				activeSubs[id]();
+				delete activeSubs[id];
+			}
+		});
+
+		Object.keys(currentSubs).forEach(id => {
+			activeSubs[id] = currentSubs[id](dispatch);
+		});
+
+		return a;
+	};
+};
