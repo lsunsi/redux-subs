c5c7c5ee849e4ccc63ccfb3d61bc883530ad3b6c
Alexander Fell <carrymass@gmail.com>
2021-05-08 21:41:43 +0100

    feat: update deps
    Updates the dependencies and makes necessary changes for types and xo

--- source/index.test.ts
+++ source/index.test.ts
@@ -1,9 +1,36 @@
 import test from 'ava';
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, Dispatch, Action, Reducer, Middleware} from 'redux';
 import install from '.';
 
-test(t => {
-	const actions = [
+const validStates = <const>['init', 'sub0', 'sub1', 'sub2', 'sub3'];
+type States = (typeof validStates)[number];
+
+type State = States | undefined;
+
+type ActionType =
+	States
+	| 'sub1-enabled'
+	| 'sub2-enabled'
+	| 'sub1-disabled'
+	| 'sub2-disabled'
+	| 'sub1-enabled'
+	| 'sub1-disabled'
+	| 'sub2-enabled'
+	| 'sub1-enabled';
+
+interface TestAction extends Action<ActionType> {}
+
+type TestDispatch = Dispatch<TestAction>;
+type DispatchExt = Record<string, unknown>;
+
+function isStateAction(type: ActionType): type is States {
+	return validStates.includes(type as States);
+}
+
+test('middleware', t => {
+	const receivedActions: ActionType[] = [];
+
+	const actions: TestAction[] = [
 		{type: 'init'},
 		{type: 'sub0'},
 		{type: 'sub1'},
@@ -11,50 +38,61 @@ test(t => {
 		{type: 'sub3'}
 	];
 
-	const expectations = [
+	const expectations: ActionType[] = [
 		'init',
-		'sub1-enabled', 'sub2-enabled',
+		'sub1-enabled',
+		'sub2-enabled',
 		'sub0',
-		'sub1-disabled', 'sub2-disabled',
+		'sub1-disabled',
+		'sub2-disabled',
 		'sub1',
 		'sub1-enabled',
 		'sub2',
-		'sub1-disabled', 'sub2-enabled',
+		'sub1-disabled',
+		'sub2-enabled',
 		'sub3',
 		'sub1-enabled'
 	];
 
-	const reducer = (state: string, {type}: any) => type.length === 4 ? type : state;
+	const reducer: Reducer<State, TestAction> = (state, {type}) => isStateAction(type) ? type : state;
 
-	const sub1 = (dispatch: any) => {
+	const sub1 = (dispatch: TestDispatch) => {
 		dispatch({type: 'sub1-enabled'});
 
 		return () => dispatch({type: 'sub1-disabled'});
 	};
 
-	const sub2 = (dispatch: any) => {
+	const sub2 = (dispatch: TestDispatch) => {
 		dispatch({type: 'sub2-enabled'});
 
 		return () => dispatch({type: 'sub2-disabled'});
 	};
 
-	const subs = (state: any) =>
-		state === 'sub0' ? {} :
-		state === 'sub1' ? {sub1} :
-		state === 'sub2' ? {sub2} :
-		{sub1, sub2};
+	const subs = (state: State) => {
+		switch (state) {
+			case 'sub0':
+				return {};
+			case 'sub1':
+				return {sub1};
+			case 'sub2':
+				return {sub2};
+			default:
+				return {sub1, sub2};
+		}
+	};
 
-	const spy = (_: any) => (next: any) => (action: any) => {
-		t.is(action.type, expectations.shift());
-
+	const spy: Middleware<DispatchExt, State, TestDispatch> = _ => next => action => {
+		const {type} = action as TestAction;
+		receivedActions.push(type);
 		return next(action);
 	};
 
-	const store = createStore(reducer, compose(
-		applyMiddleware(install(subs)),
-		applyMiddleware(spy)
-	));
+	const store = createStore(reducer,
+		applyMiddleware(spy, install(subs))
+	);
+	for (const action of actions) {
+		store.dispatch(action);
+	}
 
-	t.plan(expectations.length);
-	actions.forEach(store.dispatch);
+	t.deepEqual(receivedActions, expectations);
 });
